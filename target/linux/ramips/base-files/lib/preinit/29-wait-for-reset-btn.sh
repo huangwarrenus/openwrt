756a7ec208fad127e32a1b5898e64791afe4aa5d
Luis Soltero <lsoltero@globalmarinenet.com>
2013-06-03 21:19:54 -0400

    29-wait-for-reset-btn now really works.  The boot_hook_add was missing so the script was running before the environment had been setup.
    also added missing poray routers.
    
    Signed-off-by: Luis Soltero <lsoltero@globalmarinenet.com>

--- target/linux/ramips/base-files/lib/preinit/29-wait-for-reset-btn.sh
+++ target/linux/ramips/base-files/lib/preinit/29-wait-for-reset-btn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# on the Poray X5 and on the Toplink AP01 boards the reset switch enters a 20ms undetermined state
+# on the Poray and Toplink AP01 boards the reset switch enters a 20ms undetermined state
 # which causes a reset switch event in the preinit failsafe script which in turn triggers a
 # boot into failsafe mode. So the systems never fully boot.  A short delay is required for the 
 # reset switch to settle before the failsafe script is exectluted.  This script must be exectuted
@@ -8,11 +8,20 @@
 #
 # 2 seconds is more than enough time since the transient is on 20ms in duration.
 
+. /lib/functions.sh
 . /lib/ramips.sh
 
-case $(ramips_board_name) in
-   x5 |\
-   top-ap01)
-	sleep 2
-	;;
-esac
+fs_wait_for_poray_reset() {
+    case $(ramips_board_name) in
+       m3 |\
+       m4 |\
+       x5 |\
+       x8 |\
+       top-ap01)
+    	    echo "Wait for reset button on $(ramips_board_name) to settle"
+	    sleep 2
+	    ;;
+    esac
+}
+
+boot_hook_add preinit_main fs_wait_for_poray_reset
